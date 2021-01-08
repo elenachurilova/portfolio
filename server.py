0bfbf4250dca54e05319136ab2d709cd990a3cd4
Elena Churilova <ellena.churilova@gmail.com>
2021-01-07 17:03:53 -0800

    Initial commit

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from flask import (Flask, render_template, request, flash, session,
-                   redirect)
+                   redirect, jsonify)
 
 app = Flask(__name__)
 app.secret_key = "dev"
 
 @app.route("/")
 def hello():
-    return render_template("root.html")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
